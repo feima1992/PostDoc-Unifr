aa3ad46a2f3a44f420f432f273cd232838fc3914
FeiMa <mafei19920713@gmail.com>
2023-10-11 17:28:55 +0200

    New videoDownSample.m

--- WFanalysis/Script/Analysis20230910.m
+++
@@ -1,4 +0,0 @@
-%% Get diff map for mouse m1691, m1694
-objActReg = FileTableActReg().Filter('mouse',{'m1693','m1694'},'mvtDir',0, 'nTrial', 'All').LoadDeltaFoverF();
-
-filePairAct = FileTableActPair( 'allMethod', objActReg).CalPairDiffMap();
